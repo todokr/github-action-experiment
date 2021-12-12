fa41674609920c836eb973e7b6530df74d0d9cfc
Shunsuke Tadokoro <shunsuke.tadokoro@bizreach.co.jp>
2021-12-12 18:29:39 +0900

    fix

--- migration/test3.sql
+++ migration/test3.sql
@@ -1,2 +1,3 @@
-create table test3(varchar id not null, varchar name);
+create table test3(varchar id not null, varchar name, int age);
 alter table test3 drop column name;
+alter table test3 drop column age;
